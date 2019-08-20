5cee4271e5e57ceee3df08b3f2271cbe71d25a92
Bernardo <harman_kardon27@hotmail.com>
2019-08-20 11:47:52 -0700

    Added post backend point

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/api/posts', (req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods",
+                "GET, PUT, PATCH, DELETE, OPTIONS");
+  next();
+});
+
+app.post("/api/posts",(req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added good'
+  });
+
+});
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     { id: '4234234',
       title: 'First Title',
-      content:'fdewfewfer'},
+      content:'This is my first post from my first content'},
       { id: '345345',
-      title: '34534 Title',
-      content:'345345345345345'}
+      title: 'Second Title',
+      content:'This is another thing from another book'}
   ];
 
    res.status(200).json({

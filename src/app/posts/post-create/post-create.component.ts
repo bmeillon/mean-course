f10907dff325bc5fff675c61b9ddf2dc19daf526
Bernardo <harman_kardon27@hotmail.com>
2019-08-23 10:44:03 -0700

    Added image controls to store image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -30,6 +30,7 @@ export class PostCreateComponent implements OnInit {
     this.form = new FormGroup({
       title: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
       content: new FormControl(null, {validators: [Validators.required]}),
+      image: new FormControl(null, {validators: [Validators.required]})
     });
 
 
@@ -58,6 +59,14 @@ export class PostCreateComponent implements OnInit {
   }
 
   /* @Output() postCreated = new EventEmitter<Post>(); */
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
+  }
 
   onSavePost() {
     if (this.form.invalid) {

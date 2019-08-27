863df6820bc17993abb1d69a0637874f2133c1c3
Bernardo <harman_kardon27@hotmail.com>
2019-08-27 11:56:03 -0700

    Added updating posts with images

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -47,11 +47,12 @@ export class PostCreateComponent implements OnInit {
           this.post = { id: postData._id,
                         title: postData.title,
                         content: postData.content,
-                        imagePath: null
+                        imagePath: postData.imagePath
                       };
           this.form.setValue({
             title: this.post.title,
-            content: this.post.content
+            content: this.post.content,
+            image: this.post.imagePath
           });
         });
       } else {
@@ -89,7 +90,8 @@ export class PostCreateComponent implements OnInit {
       this.postService.updatePost(
         this.postId,
         this.form.value.title,
-        this.form.value.content);
+        this.form.value.content,
+        this.form.value.image);
     }
 
 

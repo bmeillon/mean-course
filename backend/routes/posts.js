d34a2f253a399ebb401bcf752719be245bac4a83
Bernardo <harman_kardon27@hotmail.com>
2019-09-19 12:56:15 -0700

    Added controllers

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const Post = require('../models/post');
-const multer = require('multer');
 
-const checkAuth = require('../middleware/checkout-auth');
+const PostsController = require('../controllers/posts');
 
 const router = express.Router();
+
+const checkAuth = require('../middleware/checkout-auth');
+const multer = require('multer');
 
 const MIME_TYPE_MAP = {
   'image/png' : 'png',
@@ -29,108 +30,16 @@ const storage = multer.diskStorage({
 
 });
 
+router.post("", checkAuth,
+multer({storage: storage}).single("image"), PostsController.createPost);
 
-router.post("",
-checkAuth,
-multer({storage: storage}).single("image"), (req, res, next) => {
-  const url =req.protocol + '://' + req.get("host");
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
-    creator: req.userData.userId
-  });
+router.get("", PostsController.getPost);
 
+router.get("/:id", PostsController.getPostById);
 
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added good',
-      post: {
-        ...createdPost,
-        id: createdPost._id,
+router.delete("/:id", checkAuth, PostsController.deletePostById);
 
-      }
-    });
-  });
-});
-
-router.get("", (req, res, next) => {
-  // Pagination
-  const pageSize = +req.query.pagesize;
-  const currentPage = +req.query.page;
-  const postQuery = Post.find();
-  let fetchedPosts;
-  if (pageSize && currentPage) {
-    postQuery
-    .skip(pageSize * (currentPage - 1))
-    .limit(pageSize);
-  }
-
-  postQuery
-    .then(documents => {
-      fetchedPosts = documents;
-      return Post.count();
-    })
-    .then(count => {
-      res.status(200).json({
-        message: 'Posts Fetched Succesfully',
-        posts: fetchedPosts,
-        maxPosts: count
-      });
-    });
-});
-
-router.get("/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if(post){
-      res.status(200).json(post);
-    }
-    else{
-      res.status(404).json({ message: "Not found post!"});
-    }
-  })
-});
-
-router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({_id: req.params.id, creator: req.userData.userId}).then(result => {
-    if (result.n > 0) {
-      res.status(200).json({message: "Deletion Succesfully"});
-    }
-    else {
-      res.status(401).json({message: "Not authorized"});
-    }
-  });
-
-});
-
-router.put("/:id",
-checkAuth,
-multer({storage: storage}).single("image"),
-(req,res, next) => {
-  // Si tenemos un archivo, este if nos valida ponerle un nombre
-  let imagePath = req.body.imagePath;
-  if (req.file) {
-    const url = req.protocol + "://" + req.get("host");
-    imagePath = url + "/images/" + req.file.filename;
-  }
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: imagePath,
-    creator: req.userData.userId
-  });
-  console.log(post);
-  Post.updateOne({_id: req.params.id, creator: req.userData.userId}, post).then(result => {
-    if (result.nModified > 0) {
-      res.status(200).json({message: "Updated Succesfully"});
-    }
-    else {
-      res.status(401).json({message: "Not authorized"});
-    }
-
-  });
-});
+router.put("/:id", checkAuth, multer({storage: storage}).single("image"), PostsController.updatePostById);
 
 
 module.exports = router;
